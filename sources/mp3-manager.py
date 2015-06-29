dff31291fd422fbf50ea9f8d4288bb2c5fdc3a6e
Christina Ford <chrisamethyst@gmail.com>
2015-06-29 10:53:18 -0400

    fixing spaces and lines problems

--- sources/mp3-manager.py
+++ sources/mp3-manager.py
@@ -1,19 +1,19 @@
-""" 
-    mp3-manager.py: Finds MP3s in a given directory, places 
-    non-duplicates in a new folder and tars them. 
+"""
+    mp3-manager.py: Finds MP3s in a given directory, places
+    non-duplicates in a new folder and tars them.
     
-    Copyright (C) 2015 Christina Ford 
-
+    Copyright (C) 2015 Christina Ford
+    
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
+    
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
+    
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>. """
 
@@ -29,7 +29,7 @@ import tarfile
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
-    print "Creating ARCHIVE in: %s%s", str(archname), str(source_dir) 
+    print "Creating ARCHIVE in: %s%s", str(archname), str(source_dir)
 
 def get_hash(filename):
     with open(filename, "rb") as f:
@@ -45,40 +45,40 @@ def make_dir(path):
         if excepton.errno != errno.EEXIST:
             raise
 
+
+def copy_file(hash, hash_list, dir_from, dir_to, mp3):
+    if hash not in hash_list:
+        shutil.copy(dir_from, dir_to)
+        music_hashes.append(hash)
+        print "adding %s to music list" % str(mp3)
+        return True
+    else:
+        print "Duplicate file %s not added." %str(mp3)
+        return False
+
 #traverse    directories adding mp3's to new folder
-#hash the files and add hash to list 
+#hash the files and add hash to list
 #only add the files with given extension (example .mp3)
 def traverse_dir(orig_loc, ext):
-	music_hashes = []
-	for root, dirs, music_files in os.walk(orig_loc):
-		path = root.split('/')
-		for mp3 in music_files:
-			#make sure these are mp3s
-			tmp_ext = os.path.splitext(mp3)[1]
-			if tmp_ext == ext:
-			#check for duplicates
-				m_path = os.path.realpath(os.path.join(root,mp3))
-                    
-                h = get_hash(m_path)
-                    
-                    #with open(m_path, "rb") as f:
-					#data = f.read()
-                    #h =  hashlib.sha256(data).hexdigest()
+    music_hashes = []
+    for root, dirs, music_files in os.walk(orig_loc):
+        path = root.split('/')
+        for mp3 in music_files:
+            #make sure these are mp3s
+            tmp_ext = os.path.splitext(mp3)[1]
+            if tmp_ext == ext:
+                    m_path = os.path.realpath(os.path.join(root,mp3))
+                    h = get_hash(m_path)
+                    result = copy_file(h,music_hashes, m_path, end_loc, mp3)
+            
+            else:
+                print "NOT ADDING file %s with extension: "%str(mp3), tmp_ext
 
-                    if h not in music_hashes:
-						shutil.copy(m_path, end_loc)
-						music_hashes.append(h)
-						print "adding %s to music list" % str(mp3)
-					else:
-						print "DUPLICATE FILE %s NOT ADDED." % str(mp3)
-			else:
-				print "NOT ADDING file %s with extension: "%str(mp3), tmp_ext
-	
-	print "added %d files to Directry"% len(music_hashes)
+        print "added %d files to Directry"% len(music_hashes)
 
 
 if __name__ == "__main__":
-
+    
     usage = "[Source Folder] [Destination Folder] [Archive Name] ([file extension])"
     
     if len(sys.argv) < 4 or len(sys.argv) > 5:
@@ -91,13 +91,13 @@ if __name__ == "__main__":
     else:
         ext = ".mp3"
     
-    orig_loc = sys.argv[1] 
-    end_loc  = sys.argv[2] 
+    orig_loc = sys.argv[1]
+    end_loc  = sys.argv[2]
     arch_loc = sys.argv[3]
-    
+
     make_dir(end_loc)
     # should probably check to see if this directory exists
-    
+
     if os.path.isdir(orig_loc):
         traverse_dir(orig_loc, ext)
         make_tarfile(arch_loc, end_loc)
